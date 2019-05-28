6f5a62869929fe735032f917f5ff4175fdb73e00
sharky <zeebing@gmail.com>
2019-05-28 13:19:32 -0400

    made image component use react Component class, created handleClick function

--- src/components/img.js
+++ src/components/img.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import Game from "../pages/game";
 
-export default function Img(props) {
-    return (
-        <img src={props.src} alt={props.src}></img>
-    )
+class Image extends Component {
+    state = {
+        clicked: false,
+        src: this.props.src
+    }
+    handleClick = event => {
+        console.log("click");
+        event.preventDefault();
+        this.clicked ? Game.endGame() : this.setState({ clicked: true });
+
+    }
+
+    render() {
+        return (
+            <img src={this.props.src} alt={this.props.src} onClick={this.handleClick}></img> 
+        )
+    }
+
+
 }
+
+export default Image;
+// export default function Img(props) {
+//     handleClick = (event) => {
+
+//         this.setState({play: false});
+//         this.clicked ? Game.endGame() : event.target.setState({ clicked: true });
+
+//     }
+
+//     return (
+//         <img src={props.src} alt={props.src} onClick={handleClick()}></img> 
+//     )
+// }
