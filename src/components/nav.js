4a9f718dbe7c5f243965008d7e8db18dc4625c4a
sharky <zeebing@gmail.com>
2019-05-28 21:49:14 -0400

    added score and high score

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function Nav() {
+export default function Nav(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <a className="navbar-brand" href="/">
             Clicky Game
           </a>
+          Score: {props.currentScore}
+          High Score: {props.highScore}
         </nav>
       );
 }
