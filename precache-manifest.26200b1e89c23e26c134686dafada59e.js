4b8069a1695dd73db6c3db447466c7b9cc96df7e
sharky <zeebing@gmail.com>
2019-05-28 22:32:16 -0400

    Updates

---
+++ precache-manifest.26200b1e89c23e26c134686dafada59e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49f22a77c4e117e943455f3e2a2ff97",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "02d190d2c96d979e3d1f",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2179aef72c5a257051fb",
+    "url": "/clicky-game/static/js/2.50de2f26.chunk.js"
+  },
+  {
+    "revision": "02d190d2c96d979e3d1f",
+    "url": "/clicky-game/static/js/main.7f56485c.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
