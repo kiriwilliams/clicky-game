33edfea799d687e2b248325a36ae1d0cc2dbea46
sharky <zeebing@gmail.com>
2019-05-28 21:47:26 -0400

    removed unused commits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from "./components/nav";
 import Game from "./pages/game";
 
 function App() {
   return (
     <div>
-      <Nav />
       <Game />
     </div>
   );
